b7781dfa21e383c46a2c376d7acfda54295c715f
david-swift <haenerdavid@icloud.com>
2023-08-05 16:26:24 +0200

    Use the default macOS menu for toolbar menus in every macOS version Thanks to @longseespace for providing the necessary code 🎉

--- Contributors.md
+++ Contributors.md
@@ -1,3 +1,4 @@
 # Contributors
 
 - [david-swift](https://github.com/david-swift)
+- [Daniel Nguyen](https://github.com/longseespace)
--- Sources/ColibriComponents/Model/Data/ToolbarMenu.swift
+++ Sources/ColibriComponents/Model/Data/ToolbarMenu.swift
@@ -3,6 +3,7 @@
 //  ColibriComponents
 //
 //  Created by david-swift on 02.08.2023.
+//  Edited by Daniel Nguyen on 05.08.2023.
 //
 
 import SFSafeSymbols
@@ -43,62 +44,26 @@ public struct ToolbarMenu<Content>: ToolbarActionProtocol where Content: View {
         self.init(title, symbol: .init(systemSymbol: systemSymbol), content: content)
     }
 
-    /// A menu view for macOS 12.
-    private struct ToolbarMenuView<Label, ContentView>: View where Label: View, ContentView: View {
-
-        /// Whether the menu is presented.
-        @State private var isPresented = false
-        /// The menu button's label.
-        var label: Label
-        /// The menu's content.
-        var content: ContentView
-        /// Additional padding.
-        var padding: Edge.Set
-
-        /// The view's body.
-        var body: some View {
-            Button {
-                isPresented.toggle()
-            } label: {
-                let offset = 10.0
-                label
-                    .offset(y: offset)
-                    .popover(isPresented: $isPresented) {
-                        content
-                            .buttonStyle(CustomToolbarMenu())
-                            .padding(.customToolbarPadding)
-                    }
-                    .offset(y: -offset)
-                    .customToolbarItem(padding: padding)
-            }
-        }
-
-    }
-
     /// The action's view.
     /// - Parameter padding: The horizontal padding around the button.
     /// - Returns: A view containing the action button.
     public func body(padding: Edge.Set) -> AnyView {
-        let label = Label {
-            Text(title)
-        } icon: {
-            icon
-        }
-
-        return .init(
-            Group {
-                if #available(macOS 13, *) {
-                    Menu {
-                        content
-                    } label: {
-                        label
-                            .customToolbarItem(padding: padding)
-                    }
-                    .menuStyle(.button)
-                } else {
-                    ToolbarMenuView(label: label, content: content, padding: padding)
+        .init(
+            Menu {
+                content
+            } label: {
+                Label {
+                    Text(title)
+                } icon: {
+                    icon
                 }
+                .customToolbarItem(padding: padding)
+                .border(.red)
             }
+            .menuStyle(.borderlessButton)
+            .menuIndicator(.hidden)
+            .frame(width: .customToolbarSideLength)
+            .padding(padding, .customToolbarPadding)
             .buttonStyle(CustomToolbarButton())
             .help(title)
         )
